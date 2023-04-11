ff73619e0822e04efbb3b8bd5b8547524109974b
christopher <oigoc60@gmail.com>
2023-04-11 14:20:32 +0300

    Initial commit

---
+++ src/test/java/com/warehouse/warehouseManagementSystem/WarehouseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warehouse.warehouseManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
