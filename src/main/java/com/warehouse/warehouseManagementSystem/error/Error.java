9eba9a649d501943b352c4e5135887e833b38ec5
christopher <oigoc60@gmail.com>
2023-04-17 17:28:31 +0300

    commit

--- src/main/java/com/warehouse/warehouseManagementSystem/error/Error.java
+++ src/main/java/com/warehouse/warehouseManagementSystem/error/Error.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class Error {
+    //error handling
     @ExceptionHandler(RuntimeException.class)
     public ProblemDetail problemDetail(RuntimeException exception){
         return ProblemDetail.forStatusAndDetail
